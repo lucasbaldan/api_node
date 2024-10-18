ef9abbedfebf9870fb753c70e6c378df8674fc4d
lucasbaldan <lucasbaldan2014@gmail.com>
2024-10-18 16:37:14 -0300

    melhorado controller de cidade e adicionado validação de entrada da API com a lib YUP

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { server } from "./server/Server";
 import 'dotenv/config';
 
-server.listen(process.env.PORT||80, () => console.log('App Rodando'));
+server.listen(process.env.PORT||80, () => console.log('Server ON'));
--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,7 +1,39 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 
-export const create = (req: Request, res: Response) => {
+interface ICidades {
+    nome: string,
+    uf: number,
+    codigoIBGE: number
+}
 
-    console.log('chegou na controller de create de cidades');
+const validaCampos: yup.ObjectSchema<ICidades> = yup.object().shape({
+    nome: yup.string().required().min(4),
+    uf: yup.number().required(),
+    codigoIBGE: yup.number().required("Atributo de entrada --codigoIbge-- não informado")
+})
 
+export const create = async (req: Request<{}, {}, ICidades>, res: Response) => {
+    let cidadeValidada: ICidades | undefined = undefined;
+
+    try {
+        cidadeValidada = await validaCampos.validate(req.body, { abortEarly: false });
+        console.log('Cidade validada com sucesso');
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+        const errosCampos: Record<string, string> = {};
+
+        yupError.inner.forEach(error => {
+            if (!error.path) return;
+
+            errosCampos[error.path] = error.message;
+        });
+
+        res.status(StatusCodes.BAD_REQUEST).json({
+            status: false,
+            errors: errosCampos
+        })
+
+    }
 }
